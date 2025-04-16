39b7da9e60a6336c16be2e32fae271e302f5409f
luxoranova <luxoranova9@gmail.com>
2025-04-16 22:50:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/app/page.tsx:130:30
    > Parsing ecmascript source code failed
    >   128 |
    >   129 |
    > > 130 |                   Subheadline:
    >       |                              ^
    >   131 |
    >   132 |
    >   133 |                   Feature Descriptions:
    >
    > Expected ',', got ':'
    >
    >

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -124,16 +124,16 @@ const Page = () => {
             
               
                 
-                  Headline:
+                  <span>Headline:</span>
                 
                 
-                  Subheadline: {analysisResult.description}
+                  <span>Subheadline:</span>
                 
                 
-                  Feature Descriptions:
+                  <span>Feature Descriptions:</span>
                 
                 
-                  Call to Action:
+                  <span>Call to Action:</span>
                 
               
               
